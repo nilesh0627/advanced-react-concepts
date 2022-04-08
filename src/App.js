d7d7566d8eab85273e607622bf6a1490e29aa0cf
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2022-04-08 12:20:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Timer from "./AnimatedTimer2";
+import Timer from "./SammedTimer";
 function App() {
   return (
     <div className="App">
--- src/SammedTimer/index.js
+++ src/SammedTimer/index.js
@@ -1,66 +1,84 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { getRemainingTime, arrayRotate } from './utils'
 import './styles.scss'
 
-const minutesTensArray = [5, 4, 3, 2, 1, 0];
-const minutesOnesArray = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-const secondsTensArray = [5, 4, 3, 2, 1, 0];
-const secondsOnesArray = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-
 function Timer({ finishTimer }) {
+
     const { days, hours, minutes, seconds } = getRemainingTime(finishTimer);
+    console.log(' days', days, ' hours', hours, ' minutes', minutes, ' seconds', seconds)
 
-    const minutesTensDigit = minutes / 10;
+    let _minutes_tens;
+    let _minutes_ones;
+    let _seconds_tens;
+    let _seconds_ones;
 
+    const minutesTensArray = [5, 4, 3, 2, 1, 0];
+    const minutesOnesArray = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    const secondsTensArray = [5, 4, 3, 2, 1, 0];
+    const secondsOnesArray = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+    const minutesTensDigit = Math.floor(minutes / 10);
     const minutesOnesDigit = minutes % 10;
-
-    const secondsTensDigit = seconds / 10;
-
+    const secondsTensDigit = Math.floor(seconds / 10);
     const secondsOnesDigit = seconds % 10;
 
+    console.log(minutesTensDigit, minutesOnesDigit, secondsTensDigit, secondsOnesDigit);
+
+    const timeNow = Date.now();
     const minutesTensList = arrayRotate(minutesTensArray, 5 - minutesTensDigit).map(number => {
-        return <span className="digit">{number}</span>;
+        return <span key={`minutesTensList-${timeNow + number}`} className="digit">{number}</span>;
     })
 
     const minutesOnesList = arrayRotate(minutesOnesArray, 9 - minutesOnesDigit).map(number => {
-
-        return <span className="digit">{number}</span>;
-
+        return <span key={`minutesOnesList-${timeNow + number}`} className="digit">{number}</span>;
     })
 
     const secondsTensList = arrayRotate(secondsTensArray, 5 - secondsTensDigit).map(number => {
-        return <span className="digit">{number}</span>;
+        return <span key={`secondsTensList-${timeNow + number}`} className="digit">{number}</span>;
     })
 
     const secondsOnesList = arrayRotate(secondsOnesArray, 9 - secondsOnesDigit).map(number => {
-
-        return <span className="digit">{number}</span>;
-
+        return <span key={`secondsOnesList-${timeNow + number}`} className="digit">{number}</span>;
     })
 
+    useEffect(() => {
+        _minutes_tens.style.animationDelay = `${0}s`;
+        _minutes_ones.style.animationDelay = `${seconds - 59}s`;
+        _seconds_tens.style.animationDelay = `${secondsOnesDigit - 9}s`;
+        // _seconds_ones.style.animationDelay = `${secondsOnesDigit - 10}s`;
+    }, [])
 
+    console.log('rendering');
     return (
         <div className='wrapper'>
             <div className='time-part-wrapper'>
                 <div className='time-part minutes tens'>
-                    <div className='digit-wrapper'>
+                    <div ref={(el) => {
+                        _minutes_tens = el;
+                    }} className='digit-wrapper'>
                         {minutesTensList}
                     </div>
                 </div>
                 <div className='time-part minutes ones'>
-                    <div className='digit-wrapper'>
+                    <div ref={(el) => {
+                        _minutes_ones = el;
+                    }} className='digit-wrapper'>
                         {minutesOnesList}
                     </div>
                 </div>
             </div>
             <div className='time-part-wrapper'>
                 <div className='time-part seconds tens'>
-                    <div className='digit-wrapper'>
+                    <div ref={(el) => {
+                        _seconds_tens = el;
+                    }} className='digit-wrapper'>
                         {secondsTensList}
                     </div>
                 </div>
                 <div className='time-part seconds ones'>
-                    <div className='digit-wrapper'>
+                    <div ref={(el) => {
+                        _seconds_ones = el;
+                    }} className='digit-wrapper'>
                         {secondsOnesList}
                     </div>
                 </div>
